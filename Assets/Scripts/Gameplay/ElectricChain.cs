af77c1784117efcdc8ec88d284248a999b7bc171
Bombur40 <wonyonglee616@gmail.com>
2023-09-22 17:20:05 -0700

    WIP

---
+++ Assets/Scripts/Gameplay/ElectricChain.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElectricChain : MonoBehaviour
+{
+    [SerializeField] private int _chainCount;
+    [SerializeField] private LineRenderer _electricLine;
+
+    public void ChainToNextTarget()
+    {
+
+    }
+}
