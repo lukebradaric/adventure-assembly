8c1e15637114d880114e8c9cdb323ba415d7b77a
Luke <lukebradaric@gmail.com>
2023-09-27 17:03:38 -0700

    lootbox spawn pos fix

--- Assets/Scripts/Entities/Characters/LootManager.cs
+++ Assets/Scripts/Entities/Characters/LootManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class LootManager : MonoBehaviour
 {
     [SerializeField] private GameObject _lootPrefab;
+    [SerializeField] private Vector4 _spawnBounds;
     [SerializeField] private int _dropRange;
 
     private void OnEnable()
@@ -17,10 +18,23 @@ public class LootManager : MonoBehaviour
 
     private void OnLeveledUp()
     {
-        Bounds bounds = CharacterManager.GetBounds();
-        Vector2 spawnPos = new Vector2(Random.Range((int)bounds.min.x, (int)bounds.max.x + 1), Random.Range((int)bounds.min.y, (int)bounds.max.y + 1));
-        spawnPos += new Vector2(Random.Range(-_dropRange, _dropRange + 1), Random.Range(-_dropRange, _dropRange + 1));
+        Vector2Int spawnPos = GetRandomPosition();
 
-        Instantiate(_lootPrefab, spawnPos, Quaternion.identity);
+        while (CharacterManager.TryGet(spawnPos, out Character character))
+        {
+            spawnPos = GetRandomPosition();
+        }
+
+        Instantiate(_lootPrefab, (Vector2)spawnPos, Quaternion.identity);
+    }
+
+    private Vector2Int GetRandomPosition()
+    {
+        return new Vector2Int(Random.Range((int)-_spawnBounds.x, (int)_spawnBounds.y), Random.Range((int)-_spawnBounds.z, (int)_spawnBounds.w));
+        //Vector2Int spawnPo
+        //Bounds bounds = CharacterManager.GetBounds();
+        //Vector2Int spawnPos = new Vector2Int(Random.Range((int)bounds.min.x, (int)bounds.max.x + 1), Random.Range((int)bounds.min.y, (int)bounds.max.y + 1));
+        //spawnPos += new Vector2Int(Random.Range(-_dropRange, _dropRange + 1), Random.Range(-_dropRange, _dropRange + 1));
+        //return spawnPos;
     }
 }
