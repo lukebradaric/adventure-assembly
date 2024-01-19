b229d43dd3e7f34b1a920212bb5f95eaa2077558
Luke <lukebradaric@gmail.com>
2024-01-19 15:23:18 -0800

    Added various hero sprites, WIP ability framework

---
+++ Assets/Scripts/Gameplay/Abilities/Ability.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace AdventureAssembly.Gameplay.Abilities
+{
+    public abstract class Ability
+    {
+        [Tooltip("How often should this ability occur?")]
+        [SerializeField] private int _turns;
+    }
+}
